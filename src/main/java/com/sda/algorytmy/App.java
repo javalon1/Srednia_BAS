1f310befedbba271bc8dde19ce766c162cee1b6c
bart7801 <bart7801@o2.pl>
2019-05-12 20:37:14 +0100

    srednia arytmetyczna

---
+++ src/main/java/com/sda/algorytmy/App.java
@@ -0,0 +1,26 @@
+//liczenie sredniej arytmetycznej, pierwsza dana mowi o tym ile razy wpiszemy dena do
+//obliczania sredniej arytmetycznej
+package com.sda.algorytmy;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int wynik = 0;
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            wynik = wynik + a;
+
+        }
+            wynik = wynik / n;
+        System.out.println(wynik);
+
+    }
+}
